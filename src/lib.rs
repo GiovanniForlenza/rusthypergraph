22bdf1769552fea808b800244c9c5149b8f91f07
GiovanniForlenza <giovanni.forlenza.gf@gmail.com>
2024-10-15 23:00:25 +0200

    test module core

--- src/lib.rs
+++ src/lib.rs
@@ -1,12 +1,12 @@
 use pyo3::prelude::*;
-//use pyo3::wrap_pyfunction;
 mod core;
 mod dynamics;
+
 
 #[pymodule]
 fn rusthypergraph(_py: Python, m: &PyModule) -> PyResult<()> {
     m.add_class::<core::label_encoder::LabelEncoder>()?;
     m.add_class::<core::hypergraph::Hypergraph>()?;
-    m.add_function(wrap_pyfunction!(dynamics::randwalk::random_walk, m)?)?;
+    // m.add_function(wrap_pyfunction!(dynamics::randwalk::random_walk, m)?)?;
     Ok(())
 }
