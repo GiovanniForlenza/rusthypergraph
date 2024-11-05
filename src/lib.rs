806cf18c2b815c1501888bfc8deb4397085d8f65
Giovanni Forlenza <giovanni.forlenza.gf@gmail.com>
2024-11-05 15:32:40 +0100

    add functions degree

--- src/lib.rs
+++ src/lib.rs
@@ -8,5 +8,8 @@ fn rusthypergraph(m: &Bound<'_, PyModule>) -> PyResult<()> {
     // m.add_class::<core::hypergraph::Hypergraph>()?;
     m.add_class::<core::hypergraph_wrapp::Hypergraph>()?;
     m.add_wrapped(wrap_pyfunction!(measures::degree_wrapp::degree))?;
+    m.add_wrapped(wrap_pyfunction!(measures::degree_wrapp::degree_sequence))?;
+    m.add_wrapped(wrap_pyfunction!(measures::degree_wrapp::degree_correlation))?;
+    m.add_wrapped(wrap_pyfunction!(measures::degree_wrapp::degree_distribution))?;
     Ok(())
 }
